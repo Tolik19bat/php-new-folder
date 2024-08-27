cbe8cf4b0e8d439a04eac77dfd8a86a17105a87e
Anatoly Batov <play.hoffa@gmail.com>
2024-08-27 18:08:11 +0400

    commit

---
+++ list.php
@@ -0,0 +1,8 @@
+<?php
+$array = ['apple', 'banana', 'cherry'];
+
+list($fruit1, $fruit2, $fruit3) = $array;
+
+echo $fruit1; // Выведет: apple
+echo $fruit2; // Выведет: banana
+echo $fruit3; // Выведет: cherry
